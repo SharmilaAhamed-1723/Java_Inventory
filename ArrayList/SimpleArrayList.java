70cd76199d14731316556e89870beee4c542aa2b
Sharmila Ahamed <123944740+SharmilaAhamed-1723@users.noreply.github.com>
2024-03-02 16:30:22 +0530

    Update SimpleArrayList.java

--- ArrayList/SimpleArrayList.java
+++ ArrayList/SimpleArrayList.java
@@ -20,3 +20,19 @@ public class ArrayListDemo {
     }
     
 }
+
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=0; i<5; i++){
+            list.add(sc.nextInt());
+        }
+        for(int i=0;i<5;i++) {
+            System.out.println(list.get(i) + " ");
+        }
+        System.out.print(list);
+        System.out.println();
+    }
+}
