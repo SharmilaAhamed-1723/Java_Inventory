538e9dafbcb445ecb3faa069bccc46cdf0f16d25
Sharmila Ahamed <123944740+SharmilaAhamed-1723@users.noreply.github.com>
2024-03-16 11:34:25 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -424,14 +424,7 @@ public class KKPattern {
             System.out.println();
         }
     }
-    static void pattern21(int n){
-       for(int i=1;i<=n;i++){
-           for (int j=n;j>=i;j--){
-               System.out.print(j+" ");
-           }
-           System.out.println();
-       }
-    }
+  
     
     public static void main(String[] args) {
         System.out.println("PATTERN 1");
